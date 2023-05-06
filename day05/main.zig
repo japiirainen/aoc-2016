67adec4a30c2cf05d9143d0445db911a24286fef
Joona Piirainen <joona.piirainen@gmail.com>
2023-05-06 11:16:04 +0300

    feat: solve day05 in zig
    god this was painful...

--- day05/main.zig
+++ day05/main.zig
@@ -31,15 +31,51 @@ fn part1(allocator: std.mem.Allocator, input: []const u8, debug: bool) ![]u8 {
                 std.debug.print("password = {s}\n", .{password});
                 std.debug.print("\n", .{});
             }
-            if (password.len == 8) {
+            if (password.len == 8)
                 break;
+        }
+    }
+
+    return try std.fmt.allocPrint(allocator, "{s}", .{password});
+}
+
+fn part2(allocator: std.mem.Allocator, input: []const u8, debug: bool) ![8:0]u8 {
+    var password = "????????".*;
+
+    var i: i64 = 0;
+    while (i < 100_000_000_000_000) : (i += 1) {
+        var x: [std.crypto.hash.Md5.digest_length]u8 = undefined;
+        var pw = input;
+        _ = try join(allocator, &pw, i, "{d}");
+        _ = std.crypto.hash.Md5.hash(pw, &x, .{});
+
+        var hex = try toHex(allocator, x);
+
+        if (std.mem.startsWith(u8, hex, "00000")) {
+            var idx = try std.fmt.allocPrint(allocator, "{c}", .{hex[5]});
+            var index = try std.fmt.parseInt(usize, idx, 16);
+
+            if (index < @as(u8, 8)) {
+                var atIdx = try std.fmt.allocPrint(allocator, "{c}", .{password[index]});
+                if (std.mem.eql(u8, atIdx, "?"))
+                    password[index] = hex[6];
+
+                    if (debug) {
+                        std.debug.print("iteration = {d}\n", .{i});
+                        std.debug.print("hashed = {s}\n", .{pw});
+                        std.debug.print("hex = {s}\n", .{hex});
+                        std.debug.print("hex[5] = {c}\n", .{hex[5]});
+                        std.debug.print("password = {s}\n", .{password});
+                        std.debug.print("\n", .{});
+                    }
+
+                if (!std.mem.containsAtLeast(u8, &password, 1, "?"))
+                    break;
             }
         }
     }
 
-    var b: [1024]u8 = undefined;
-
-    return try std.fmt.bufPrint(&b, "{s}", .{password});
+    return password;
 }
 
 pub fn main() !void {
@@ -48,12 +84,22 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     const input: []const u8 = "ugkcyxxp";
-    // const input: []const u8 = "abc";
-
-    const debug = true;
+    const debug = false;
 
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Solving for input : {s}\nDebug = {any}\n\n", .{input, debug});
 
-    try stdout.print("part 1 : {s}\n", .{try part1(allocator, input, debug)});
+    var timer = try std.time.Timer.start();
+
+    const p1 = try part1(allocator, input, debug);
+
+    const t0 = timer.lap() / 1000;
+    _ = timer.reset();
+
+    const p2 = try part2(allocator, input, debug);
+    const t1 = timer.lap() / 1000;
+
+    try stdout.print("----- results: -----\n", .{});
+    try stdout.print("part 1 in {d}µs : {s}\n", .{t0, p1});
+    try stdout.print("part 2 in {d}µs : {s}\n", .{t1, p2});
 }
